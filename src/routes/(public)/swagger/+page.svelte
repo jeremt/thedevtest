d8bd2b5e26630b83ddc2f246c7e44bfbf8b6bcc4
JonathanPicques <jonathan.picques@gmail.com>
2025-03-18 15:55:48 +0100

    feat: add swagger gen

---
+++ src/routes/(public)/swagger/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import SwaggerUI from 'swagger-ui';
+
+	import openapi from '$lib/swagger/swagger.json';
+
+	import 'swagger-ui/dist/swagger-ui.css';
+
+	$effect(() => {
+		SwaggerUI({ spec: openapi, dom_id: '#swagger-ui' });
+		const bg = document.querySelector('.bg') as HTMLDivElement;
+		if (bg) {
+			bg.style.display = 'none';
+			return () => {
+				bg.style.display = 'block';
+			};
+		}
+	});
+</script>
+
+<div id="swagger-ui" style:height="100vh" style:overflow="auto" style:background="white"></div>
