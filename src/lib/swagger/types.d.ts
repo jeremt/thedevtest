8fa38b859fd84f893a0f821492da0f4936f67330
JonathanPicques <jonathan.picques@gmail.com>
2025-03-27 15:45:38 +0100

    feat: remove default route

--- src/lib/swagger/types.d.ts
+++ src/lib/swagger/types.d.ts
@@ -1,42 +1,4 @@
-export interface paths {
-    "/api/stats/": {
-        parameters: {
-            query?: never;
-            header?: never;
-            path?: never;
-            cookie?: never;
-        };
-        get: {
-            parameters: {
-                query?: never;
-                header?: never;
-                path?: never;
-                cookie?: never;
-            };
-            requestBody?: never;
-            responses: {
-                /** @description OK */
-                200: {
-                    headers: {
-                        [name: string]: unknown;
-                    };
-                    content: {
-                        "application/json": {
-                            message: string;
-                        };
-                    };
-                };
-            };
-        };
-        put?: never;
-        post?: never;
-        delete?: never;
-        options?: never;
-        head?: never;
-        patch?: never;
-        trace?: never;
-    };
-}
+export type paths = Record<string, never>;
 export type webhooks = Record<string, never>;
 export interface components {
     schemas: never;
